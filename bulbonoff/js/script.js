234daac82fa262394a3dd7bdc6eb5da7b33fb1ad
Mathews Thomas <150216828+Mathews-Thomas@users.noreply.github.com>
2024-01-25 23:17:49 +0530

    Delete bulbonoff directory

--- bulbonoff/js/script.js
+++
@@ -1,6 +0,0 @@
-function bulbon(){
-    document.getElementById('image').src='https://www.w3schools.com/js/pic_bulbon.gif'
-}
-function bulboff(){
-    document.getElementById('image').src='https://www.w3schools.com/js/pic_bulboff.gif'
-}
