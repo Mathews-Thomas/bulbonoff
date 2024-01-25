57cd0e8b97baec412b39d004ce1745e62f1f4823
Mathews Thomas <150216828+Mathews-Thomas@users.noreply.github.com>
2024-01-25 23:18:38 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,6 @@
+function bulbon(){
+    document.getElementById('image').src='https://www.w3schools.com/js/pic_bulbon.gif'
+}
+function bulboff(){
+    document.getElementById('image').src='https://www.w3schools.com/js/pic_bulboff.gif'
+}
